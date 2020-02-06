4899fc4dcf2ecb4938a1708f73d15687e9960798
Alex <alextsepa001@gmail.com>
2020-02-06 18:23:59 +0200

    +error

--- Lab_3.py
+++ Lab_3.py
@@ -1,35 +1,30 @@
-# Defining our function as seidel which takes 3 arguments 
-# as A matrix, Solution and B matrix 
-   
+import numpy as np
+from numpy import linalg as la
 def seidel(a, x ,b): 
-    #Finding length of a(3)        
     n = len(a)                    
-    # for loop for 3 times as to calculate x, y , z 
     for j in range(0, n):         
-        # temp variable d to store b[j] 
         d = b[j]                   
           
-        # to calculate respective xi, yi, zi 
         for i in range(0, n):      
             if(j != i): 
-                d-=a[j][i] * x[i] 
-        # updating the value of our solution         
-        x[j] = d / a[j][j] 
-    # returning our updated solution            
+                d-=a[j][i] * x[i]        
+        x[j] = d / a[j][j]      
     return x     
-   
-# int(input())input as number of variable to be solved                 
-n = 3                              
-a = []                             
-b = []         
-# initial solution depending on n(here n=3)                      
-x = [0, 0, 0]                         
-a = [[4, 1, 2],[3, 5, 1],[1, 1, 3]] 
-b = [4,7,3] 
-print(x) 
-  
-#loop run for m times depending on m the error value 
-for i in range(0, 25):             
+                     
+x = [0, 0, 0,0,0]                         
+a = [[6.81, 1.12, 0.95, 1.165, -0.51],
+     [1.08, 3.61, 1.3, -1.63, -0.18],
+     [0.99, -2.46, 5.99, 2.1, 0.283],
+     [1.135, 0.16, 2.1, 5.55, -2],
+     [0.99, -0.48, -0.017, 1, 4]]
+b = [2.1,0.6,0.43,5.52,-0.75]
+
+error = np.dot(a, x) - b
+print(la.norm(error)) 
+
+while la.norm(error) > 0.0001:             
     x = seidel(a, x, b) 
-    #print each time the updated solution 
-    print(x)      
+    error = np.dot(a, x) - b
+    print('error = ', la.norm(error))   
+
+print(x)
